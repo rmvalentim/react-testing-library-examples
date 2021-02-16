ea91536af56be7d15f9039be2a1b6522b7d90cb3
Rafael Valentim <rmoraesvalentim@gmail.com>
2021-02-16 17:04:36 -0300

    Ultimos testes implementados

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent,  render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 import Conta from './Conta';
 
@@ -12,6 +12,18 @@ describe('Componente de conta', () => {
         const saldo = screen.getByTestId('saldo-conta');
 
         expect(saldo.textContent).toBe('R$ 1000');
+
+    });
+
+    it('Chama a função de realizar transação quando o botão é clicado', () => {
+
+        const funcaoRealizarTransacao = jest.fn()
+
+        render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransacao} />);
+
+        fireEvent.click(screen.getByText('Realizar operação'));
+
+        expect(funcaoRealizarTransacao).toHaveBeenCalled();
 
     });
 
